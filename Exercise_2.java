f29c2a7c91a26ede7d65deb225c426719d38e9b4
Piyush Chhoriya <piyushchhoriya98@gmial.com>
2024-12-23 16:44:39 -0500

    Precourse 1 exercise 2 & 3 commit

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,10 @@
-public class StackAsLinkedList { 
+// Time Complexity :O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : I was not sure how to implement it and do I need to maintain another pointer as I was cons
+// considering that there should be 2 pointers one head and top. After, I watched a video and then it became clear to me and I implemented it
+
+class StackAsLinkedList { 
   
     StackNode root; 
   
@@ -6,32 +12,57 @@ public class StackAsLinkedList {
         int data; 
         StackNode next; 
   
-        StackNode(int data) 
+        StackNode(int data)  //initialized data with value and next with null
         { 
+            this.data=data;
+            this.next=null;
             //Constructor here 
         } 
     } 
     
 	
-    public boolean isEmpty() 
+    public boolean isEmpty()  //return true if stack is empty i.e no node present
     { 
+        return root==null;
         //Write your code here for the condition if stack is empty. 
     } 
   
-    public void push(int data) 
+    public void push(int data)   //to push we will first need to create a node assigning it a data and as it will be at a top in stack we will make it as a root
     { 
+        StackNode node = new StackNode(data);
+
+        
+        node.next=root;
+        root=node;
+        
         //Write code to push data to the stack. 
     } 
   
-    public int pop() 
+    public int pop()          //Function to pop a element 
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+    if(root==null){          //if root is null then there are no elements present
+        System.out.println("Stack Underflow");
+        return 0;
+    }
+    else{                   //Pop the root as it will be the top most element and return it's value i.e data
+        int temp=root.data;
+        root=root.next;
+        return temp;
+    }
     } 
   
-    public int peek() 
+    public int peek()       // this will check if stack is empty or not if not empty then it will just return the value of the root
     { 
+        if(root==null){
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+        else{
+            return root.data;
+        }
         //Write code to just return the topmost element without removing it.
     } 
   
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,13 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this : No
+
 import java.io.*; 
   
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+ class LinkedList { 
   
     Node head; // head of list 
   
@@ -15,8 +20,10 @@ public class LinkedList {
         Node next; 
   
         // Constructor 
-        Node(int d) 
+        Node(int d)   //thios is a constructor which is initializing a node data and setting a next as null
         { 
+            this.data=d;
+            this.next=null;
             //Write your code here 
         } 
     } 
@@ -25,7 +32,18 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node node = new Node(data);                     //to insert first we are creating a node and initializing data with the value passed
+        if(list.head==null){                            //Checking if head of the list is null if so we will make the node as head
+            list.head=node;
+        }   
+        else{                                           //if head is not null then we will traverse till the end of the list and attach the new node at the end and return a list
+            Node temp=list.head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            temp.next=node;
+        }
+        return list;
         // If the Linked List is empty, 
         // then make the new node as head 
         
@@ -38,8 +56,18 @@ public class LinkedList {
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
+    public static void printList(LinkedList list)  //This function checks if list is empty if not then it prints the value of every node in the list
     {  
+        Node temp=list.head;
+        if(temp==null){
+            System.out.println("List is empty");
+        }
+        else{
+            while(temp!=null){
+                System.out.println(temp.data);
+                temp=temp.next;
+            }
+        }
         // Traverse through the LinkedList 
    
             // Print the data at current node 
